a6a2ca23a4a551ea3753b60a9db7a9700a459147
c0d3rMark <MarkM78@users.noreply.github.com>
2022-01-03 11:54:53 +1100

    Added puppy pic with JS script to change pic.

--- main.js
+++ main.js
@@ -9,3 +9,8 @@ var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
 
 document.querySelector(".vibe").append(vibe);
 
+if (vibe.includes('puppy')) {
+    var changeElem = document.getElementsByClassName('love');
+    changeElem[0].innerHTML = '<img alt="puppy sitting" height="300" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/French_bulldog_puppy_fawn.jpg">';
+};
+
