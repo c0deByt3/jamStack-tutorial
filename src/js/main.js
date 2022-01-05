0e6bc8377b083751cdbc66d5bb6dbc3335ac97a6
c0d3rMark <MarkM78@users.noreply.github.com>
2022-01-05 14:22:28 +1100

    Added eleventy config files & dependecies

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+//  Good vibe options
+
+const vibes = [
+    "...and you are awesome!",
+    "...have a wonderful day!",
+    "...and you've got this!",
+    '...and so is this puppy!'
+];
+
+// Select a random vibe
+
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+// Display random vibe in browser
+
+document.querySelector(".vibe").append(vibe);
+
+//  Display puppy picture if puppy vibe
+
+if (vibe.includes('puppy')) {
+    var changeElem = document.getElementsByClassName('love');
+    changeElem[0].innerHTML = '<img alt="puppy sitting" height="300" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/French_bulldog_puppy_fawn.jpg">';
+};
+
