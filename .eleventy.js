7b76546a9dc911feefc49965758976fe857abfe7
c0d3rMark <MarkM78@users.noreply.github.com>
2022-01-11 16:07:15 +1100

    Added JSON & Updated Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,21 @@
 module.exports = function(config) {
 
+    // config.addPassthroughCopy('src/js');
+    // return {
+    //     dir: {
+    //         input: "src",
+    //         output: "dist",
+    //     }
+    // }
+    
+    config.addPassthroughCopy('src/_data');
     config.addPassthroughCopy('src/js');
+
     return {
         dir: {
             input: "src",
             output: "dist",
         }
     };
+
 };
--- _site/src/js/main.js
+++ _site/src/js/main.js
@@ -22,9 +22,16 @@ if (vibe.includes('puppy')) {
     changeElem[0].innerHTML = '<img alt="puppy sitting" height="300" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/French_bulldog_puppy_fawn.jpg">';
 };
 
-// API data display (Access JSON Data)
+// API data display
+
+// let file = fetch("_data/news.json");
+
+// async function getText(file) {
+//     let myObject = await fetch(file);
+//     let myText = await myObject.text();
+//     console.log(myText);
+//   }
 
 let news = fetch('/_data/news.json')
-.then(response => response.json())
+.then(response => response.text())
 .then(data => console.log(data));
-
